9c90d4dd7ed0d8a120fb4cd582239b9cc23be0b1
Sergio León <sl.doncel@pm.me>
2023-04-30 18:25:44 +0200

    GPS coherence in code (not a functional change)

--- src/external_dependencies/hector_gazebo/hector_gazebo_plugins/src/gazebo_ros_gps.cpp
+++ src/external_dependencies/hector_gazebo/hector_gazebo_plugins/src/gazebo_ros_gps.cpp
@@ -14,10 +14,10 @@
 //       endorse or promote products derived from this software without
 //       specific prior written permission.
 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
 // DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
 // (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
 // LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
@@ -26,31 +26,28 @@
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //=================================================================================================
 
-#include <hector_gazebo_plugins/gazebo_ros_gps.h>
 #include <gazebo/physics/physics.hh>
+#include <hector_gazebo_plugins/gazebo_ros_gps.h>
 #include <random>
 
 // WGS84 constants
 static const double equatorial_radius = 6378137.0;
-static const double flattening = 1.0/298.257223563;
-static const double excentrity2 = 2*flattening - flattening*flattening;
+static const double flattening = 1.0 / 298.257223563;
+static const double excentrity2 = 2 * flattening - flattening * flattening;
 
 // default reference position
-static const double DEFAULT_REFERENCE_LATITUDE  = 49.9;
+static const double DEFAULT_REFERENCE_LATITUDE = 49.9;
 static const double DEFAULT_REFERENCE_LONGITUDE = 8.9;
-static const double DEFAULT_REFERENCE_HEADING   = 0.0;
-static const double DEFAULT_REFERENCE_ALTITUDE  = 0.0;
+static const double DEFAULT_REFERENCE_HEADING = 0.0;
+static const double DEFAULT_REFERENCE_ALTITUDE = 0.0;
 
 namespace gazebo {
 
-GazeboRosGps::GazeboRosGps()
-{
-}
+GazeboRosGps::GazeboRosGps() {}
 
 ////////////////////////////////////////////////////////////////////////////////
 // Destructor
-GazeboRosGps::~GazeboRosGps()
-{
+GazeboRosGps::~GazeboRosGps() {
   updateTimer.Disconnect(updateConnection);
 
   dynamic_reconfigure_server_position_.reset();
@@ -63,8 +60,7 @@ GazeboRosGps::~GazeboRosGps()
 
 ////////////////////////////////////////////////////////////////////////////////
 // Load the controller
-void GazeboRosGps::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
-{
+void GazeboRosGps::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) {
   world = _model->GetWorld();
 
   // load parameters
@@ -73,19 +69,17 @@ void GazeboRosGps::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
   else
     namespace_ = _sdf->GetElement("robotNamespace")->GetValue()->GetAsString();
 
-  if (!_sdf->HasElement("bodyName"))
-  {
+  if (!_sdf->HasElement("bodyName")) {
     link = _model->GetLink();
     link_name_ = link->GetName();
-  }
-  else {
+  } else {
     link_name_ = _sdf->GetElement("bodyName")->GetValue()->GetAsString();
     link = _model->GetLink(link_name_);
   }
 
-  if (!link)
-  {
-    ROS_FATAL("GazeboRosGps plugin error: bodyName: %s does not exist\n", link_name_.c_str());
+  if (!link) {
+    ROS_FATAL("GazeboRosGps plugin error: bodyName: %s does not exist\n",
+              link_name_.c_str());
     return;
   }
 
@@ -94,30 +88,34 @@ void GazeboRosGps::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
   fix_topic_ = "/odometry/gps";
   velocity_topic_ = "/gps_velocity";
 
-  reference_latitude_  = DEFAULT_REFERENCE_LATITUDE;
+  reference_latitude_ = DEFAULT_REFERENCE_LATITUDE;
   reference_longitude_ = DEFAULT_REFERENCE_LONGITUDE;
-  reference_heading_   = DEFAULT_REFERENCE_HEADING * M_PI/180.0;
-  reference_altitude_  = DEFAULT_REFERENCE_ALTITUDE;
+  reference_heading_ = DEFAULT_REFERENCE_HEADING * M_PI / 180.0;
+  reference_altitude_ = DEFAULT_REFERENCE_ALTITUDE;
 
   if (_sdf->HasElement("frameId"))
     frame_id_ = _sdf->GetElement("frameId")->GetValue()->GetAsString();
 
   if (_sdf->HasElement("topicName"))
-    fix_topic_ = "/odometry/gps";
-    //fix_topic_ = _sdf->GetElement("topicName")->GetValue()->GetAsString();
+    fix_topic_ = _sdf->GetElement("topicName")->GetValue()->GetAsString();
 
   if (_sdf->HasElement("velocityTopicName"))
-    velocity_topic_ = _sdf->GetElement("velocityTopicName")->GetValue()->GetAsString();
+    velocity_topic_ =
+        _sdf->GetElement("velocityTopicName")->GetValue()->GetAsString();
 
   if (_sdf->HasElement("referenceLatitude"))
     _sdf->GetElement("referenceLatitude")->GetValue()->Get(reference_latitude_);
 
   if (_sdf->HasElement("referenceLongitude"))
-    _sdf->GetElement("referenceLongitude")->GetValue()->Get(reference_longitude_);
+    _sdf->GetElement("referenceLongitude")
+        ->GetValue()
+        ->Get(reference_longitude_);
 
   if (_sdf->HasElement("referenceHeading"))
-    if (_sdf->GetElement("referenceHeading")->GetValue()->Get(reference_heading_))
-      reference_heading_ *= M_PI/180.0;
+    if (_sdf->GetElement("referenceHeading")
+            ->GetValue()
+            ->Get(reference_heading_))
+      reference_heading_ *= M_PI / 180.0;
 
   if (_sdf->HasElement("referenceAltitude"))
     _sdf->GetElement("referenceAltitude")->GetValue()->Get(reference_altitude_);
@@ -127,86 +125,103 @@ void GazeboRosGps::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
   velocity_error_model_.Load(_sdf, "velocity");
 
   // calculate earth radii
-  double temp = 1.0 / (1.0 - excentrity2 * sin(reference_latitude_ * M_PI/180.0) * sin(reference_latitude_ * M_PI/180.0));
+  double temp =
+      1.0 / (1.0 - excentrity2 * sin(reference_latitude_ * M_PI / 180.0) *
+                       sin(reference_latitude_ * M_PI / 180.0));
   double prime_vertical_radius = equatorial_radius * sqrt(temp);
   radius_north_ = prime_vertical_radius * (1 - excentrity2) * temp;
-  radius_east_  = prime_vertical_radius * cos(reference_latitude_ * M_PI/180.0);
+  radius_east_ =
+      prime_vertical_radius * cos(reference_latitude_ * M_PI / 180.0);
 
   // Make sure the ROS node for Gazebo has already been initialized
-  if (!ros::isInitialized())
-  {
-    ROS_FATAL_STREAM("A ROS node for Gazebo has not been initialized, unable to load plugin. "
-      << "Load the Gazebo system plugin 'libgazebo_ros_api_plugin.so' in the gazebo_ros package)");
+  if (!ros::isInitialized()) {
+    ROS_FATAL_STREAM(
+        "A ROS node for Gazebo has not been initialized, unable to load "
+        "plugin. "
+        << "Load the Gazebo system plugin 'libgazebo_ros_api_plugin.so' in the "
+           "gazebo_ros package)");
     return;
   }
 
   node_handle_ = new ros::NodeHandle(namespace_);
-  gps_pose_publisher_ = node_handle_->advertise<nav_msgs::Odometry>(fix_topic_, 10);
-  velocity_publisher_ = node_handle_->advertise<geometry_msgs::Vector3Stamped>(velocity_topic_, 10);
+  gps_pose_publisher_ =
+      node_handle_->advertise<nav_msgs::Odometry>(fix_topic_, 10);
+  velocity_publisher_ = node_handle_->advertise<geometry_msgs::Vector3Stamped>(
+      velocity_topic_, 10);
 
   // setup dynamic_reconfigure servers
-  dynamic_reconfigure_server_position_.reset(new dynamic_reconfigure::Server<SensorModelConfig>(ros::NodeHandle(*node_handle_, fix_topic_ + "/position")));
-  dynamic_reconfigure_server_velocity_.reset(new dynamic_reconfigure::Server<SensorModelConfig>(ros::NodeHandle(*node_handle_, fix_topic_ + "/velocity")));
-  dynamic_reconfigure_server_status_.reset(new dynamic_reconfigure::Server<GNSSConfig>(ros::NodeHandle(*node_handle_, fix_topic_ + "/status")));
-  dynamic_reconfigure_server_position_->setCallback(boost::bind(&SensorModel3::dynamicReconfigureCallback, &position_error_model_, _1, _2));
-  dynamic_reconfigure_server_velocity_->setCallback(boost::bind(&SensorModel3::dynamicReconfigureCallback, &velocity_error_model_, _1, _2));
+  dynamic_reconfigure_server_position_.reset(
+      new dynamic_reconfigure::Server<SensorModelConfig>(
+          ros::NodeHandle(*node_handle_, fix_topic_ + "/position")));
+  dynamic_reconfigure_server_velocity_.reset(
+      new dynamic_reconfigure::Server<SensorModelConfig>(
+          ros::NodeHandle(*node_handle_, fix_topic_ + "/velocity")));
+  dynamic_reconfigure_server_status_.reset(
+      new dynamic_reconfigure::Server<GNSSConfig>(
+          ros::NodeHandle(*node_handle_, fix_topic_ + "/status")));
+  dynamic_reconfigure_server_position_->setCallback(
+      boost::bind(&SensorModel3::dynamicReconfigureCallback,
+                  &position_error_model_, _1, _2));
+  dynamic_reconfigure_server_velocity_->setCallback(
+      boost::bind(&SensorModel3::dynamicReconfigureCallback,
+                  &velocity_error_model_, _1, _2));
 
   Reset();
 
   // connect Update function
   updateTimer.setUpdateRate(4.0);
   updateTimer.Load(world, _sdf);
-  updateConnection = updateTimer.Connect(boost::bind(&GazeboRosGps::Update, this));
+  updateConnection =
+      updateTimer.Connect(boost::bind(&GazeboRosGps::Update, this));
 }
 
-void GazeboRosGps::Reset()
-{
+void GazeboRosGps::Reset() {
   updateTimer.Reset();
   position_error_model_.reset();
   velocity_error_model_.reset();
 }
 
-
-
 ////////////////////////////////////////////////////////////////////////////////
 // Update the controller
-void GazeboRosGps::Update()
-{
+void GazeboRosGps::Update() {
   common::Time sim_time = world->SimTime();
   double dt = updateTimer.getTimeSinceLastUpdate().Double();
 
   ignition::math::Pose3d pose = link->WorldPose();
 
-  //ignition::math::Vector3d velocity = velocity_error_model_(link->WorldLinearVel(), dt);
-  //ignition::math::Vector3d position = position_error_model_(pose.Pos()); //,dt
+  // ignition::math::Vector3d velocity =
+  // velocity_error_model_(link->WorldLinearVel(), dt); ignition::math::Vector3d
+  // position = position_error_model_(pose.Pos()); //,dt
 
-
-  // An offset error in the velocity is integrated into the position error for the next timestep.
-  // Note: Usually GNSS receivers have almost no drift in the velocity signal.
+  // An offset error in the velocity is integrated into the position error for
+  // the next timestep. Note: Usually GNSS receivers have almost no drift in the
+  // velocity signal.
   gps_pose_.header.stamp = ros::Time(sim_time.sec, sim_time.nsec);
   velocity_.header.stamp = gps_pose_.header.stamp;
 
-  //Modificación ARP: ruido gaussiano sobre "pose.pos" del Ground Truth
-  // Definir generador de números aleatorios según distribución gaussiana, con un error de entre 0 y 1
+  // Modificación ARP: ruido gaussiano sobre "pose.pos" del Ground Truth
+  // Definir generador de números aleatorios según distribución gaussiana, con
+  // un error de entre 0 y 1
   const double mean = 0.0;
   const double stddev = 3;
-  auto dist = std::bind(std::normal_distribution<double>{mean, stddev},std::mt19937(std::random_device{}()));
+  auto dist = std::bind(std::normal_distribution<double>{mean, stddev},
+                        std::mt19937(std::random_device{}()));
   double poseX, poseY, poseZ;
   poseX = pose.X() + dist();
   poseY = pose.Y() + dist();
   poseZ = pose.Z() + dist();
 
-  //Meter pose.pos en el objeto "gps_pose_"
+  // Meter pose.pos en el objeto "gps_pose_"
   gps_pose_.header.frame_id = "world";
   gps_pose_.pose.pose.position.x = poseX;
   gps_pose_.pose.pose.position.y = poseY;
   gps_pose_.pose.pose.position.z = poseZ;
-  gps_pose_.pose.covariance[0]  = stddev*stddev;
-  gps_pose_.pose.covariance[7]  = stddev*stddev;
-  gps_pose_.pose.covariance[14] = stddev*stddev;
+  gps_pose_.pose.covariance[0] = stddev * stddev;
+  gps_pose_.pose.covariance[7] = stddev * stddev;
+  gps_pose_.pose.covariance[14] = stddev * stddev;
   /*
   Covarianza[36]:
-  00 01 02 03 04 05 
+  00 01 02 03 04 05
   06 07 08 09 10 11
   12 13 14 15 16 17
   ...
